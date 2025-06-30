f44024def361b00045b13acb73be808ebebad599
Starstreak <Starstreak1@proton.me>
2025-06-30 08:12:34 -0700

    - Test for opening file. It fails due to non implementation

--- test/tokenizer_test.py
+++ test/tokenizer_test.py
@@ -22,3 +22,11 @@ def test_object_creation(setup_resources):
     Tests that the tokenizer object is created properly.
     """
     tokenizer = setup_resources["tokenizer"]
+
+
+def test_open_file(setup_resources):
+    """
+    Test that we can open our file when an object is instantiated
+    """
+    tokenizer2 = Tokenizer()
+    assert tokenizer2.open_file is not None
