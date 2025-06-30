97a87bd48ffb04c336737522b26c9fc70cc7a3a3
Starstreak <Starstreak1@proton.me>
2025-06-30 08:07:44 -0700

    - Started test suite for tokenizer, but not actual test suite
    - Created Tokenizer class

---
+++ src/tokenizer.py
@@ -0,0 +1,8 @@
+"""
+src/tokenizer.py
+Handles tokenizing the input
+"""
+
+class Tokenizer:
+    def __init__(self):
+        pass
--- test/tokenizer_test.py
+++ test/tokenizer_test.py
@@ -1,3 +1,24 @@
 """
 The test suite for the Jack tokenizer
 """
+
+import pytest
+
+from src.tokenizer import Tokenizer
+
+@pytest.fixture
+def setup_resources():
+    """
+    Sets up the resources necessary for the tokenizer
+    """
+    tokenizer = Tokenizer()
+    yield {
+        "tokenizer": tokenizer,
+    }
+
+
+def test_object_creation(setup_resources):
+    """
+    Tests that the tokenizer object is created properly.
+    """
+    tokenizer = setup_resources["tokenizer"]
