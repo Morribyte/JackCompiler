5847e3325c7c60502260cf72c890d2abdfe71be0
Starstreak <Starstreak1@proton.me>
2025-07-02 12:16:45 -0700

    - Test now passes.

---
+++ src/compilation_engine.py
@@ -0,0 +1,12 @@
+"""
+src/compilation_engine.py
+Handles turning the tokens into XML output and writes them to file.
+"""
+
+
+class CompilationEngine:
+    """
+    Represents a compilation engine object.
+    """
+    def __init__(self):
+        pass
