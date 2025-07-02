7c29337e4e91e39a9e31d40115530bf10f369902
Starstreak <Starstreak1@proton.me>
2025-07-02 12:24:17 -0700

    - Import element tree
    - Import Tokenizer as a dependency of compilation engine

--- src/compilation_engine.py
+++ src/compilation_engine.py
@@ -3,10 +3,13 @@ src/compilation_engine.py
 Handles turning the tokens into XML output and writes them to file.
 """
 
+import xml.etree.ElementTree as element_tree
+
+from src.tokenizer import Tokenizer
 
 class CompilationEngine:
     """
     Represents a compilation engine object.
     """
-    def __init__(self):
+    def __init__(self, tokenizer: Tokenizer):
         pass
