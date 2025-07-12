edc4ce2beebca10eb4c728160abc506ceb25751f
Starstreak <Starstreak1@proton.me>
2025-07-12 14:01:21 -0700

    - Grabs command line file or path.

--- jack_analyzer.py
+++ jack_analyzer.py
@@ -4,11 +4,19 @@ Opens and writes XML files for the compiler.
 """
 
 import os
+import sys
 
 def main():
     """
     Handles the main compiler loop.
     """
+    if len(sys.argv) < 2:
+        print("Usage: python jack_analyzer.py <input path>")
+        sys.exit(1)
+
+    path = sys.argv[1]
+
+
 
 
 if __name__ == "__main__":
