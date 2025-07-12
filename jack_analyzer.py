4f61dc3c256a4347a4ff41f5c8b00ff6a8e2d7ca
Starstreak <Starstreak1@proton.me>
2025-07-12 13:58:25 -0700

    - Started writing the analyzer file.
    - Imported os.

---
+++ jack_analyzer.py
@@ -0,0 +1,15 @@
+"""
+jack_analyzer.py
+Opens and writes XML files for the compiler.
+"""
+
+import os
+
+def main():
+    """
+    Handles the main compiler loop.
+    """
+
+
+if __name__ == "__main__":
+    main()
