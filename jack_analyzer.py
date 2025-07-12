913e7205e324d9826afd85e36aaa7f797fae811b
Starstreak <Starstreak1@proton.me>
2025-07-12 14:20:56 -0700

    - Changed to Pathlib and converted the path strings to Paths.

--- jack_analyzer.py
+++ jack_analyzer.py
@@ -3,8 +3,9 @@ jack_analyzer.py
 Opens and writes XML files for the compiler.
 """
 
-import os
+from pathlib import Path
 import sys
+import xml.etree.ElementTree as element_tree
 
 from src.tokenizer import Tokenizer
 from src.compilation_engine import CompilationEngine
@@ -19,26 +20,42 @@ def check_args():
     return path
 
 
-def check_files(path):
-    if os.path.isdir(path):
+def check_files(path) -> list[Path]:
+    if path.is_dir():
         print("Found directory:")
-        files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jack")]
+        files = [f for f in path.glob("*.jack")]
         print(files)
-    elif path.endswith(".jack"):
-        print("Found file.")
+    elif path.is_file() and path.suffix == ".jack":
+        print("Found file:")
         files = [path]
         print(files)
     else:
-        raise ValueError("Invalid input: must be .jack file or directory.")
+        raise ValueError("Invalid input: must be a .jack file or a directory containing .jack files.")
+
+    return files
 
 
 def main():
     """
     Handles the main compiler loop.
     """
-    path = check_args()
-    check_files(path)
 
+    path = Path(check_args())
+    files: list[Path] = check_files(path)
+
+    for jack_files in files:
+        print(jack_files)
+        tokenizer = Tokenizer(jack_files)
+        compiler = CompilationEngine(tokenizer)
+
+        compiler.compile_class()
+
+        tree = element_tree.ElementTree(compiler.root)
+        element_tree.indent(tree)
+        tree.write(fr"{filename}.xml" if filename else "output.xml", encoding="utf-8", short_empty_elements=False)
+        xml_str = element_tree.tostring(compiler.root, encoding="unicode", method="html")
+
+        print("XML file parsed and formatted.")
 
 if __name__ == "__main__":
     main()
