e46a10117426ff47bc378b7a6f723fa40a13feae
Starstreak <Starstreak1@proton.me>
2025-07-12 14:08:55 -0700

    - Extracted the path checker into its own function.
    - Extracted args checker into its own function.

--- jack_analyzer.py
+++ jack_analyzer.py
@@ -6,18 +6,17 @@ Opens and writes XML files for the compiler.
 import os
 import sys
 
-def main():
-    """
-    Handles the main compiler loop.
-    """
+
+def check_args():
     if len(sys.argv) < 2:
         print("Usage: python jack_analyzer.py <input path>")
         sys.exit(1)
-
     path = sys.argv[1]
-
     print(f"Current path: {path}")
+    return path
 
+
+def check_files(path):
     if os.path.isdir(path):
         print("Found directory:")
         files = [os.path.join(path, f) for f in os.listdir(path) if f.endswith(".jack")]
@@ -28,5 +27,15 @@ def main():
         print(files)
     else:
         raise ValueError("Invalid input: must be .jack file or directory.")
+
+
+def main():
+    """
+    Handles the main compiler loop.
+    """
+    path = check_args()
+    check_files(path)
+
+
 if __name__ == "__main__":
     main()
